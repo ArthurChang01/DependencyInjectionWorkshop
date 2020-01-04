67ee074d0ea1f6ef4a7e4114a427e31515414831
Arthur <a802216@gmail.com>
2020-01-04 10:31:32 +0800

    fatboy

--- DependencyInjectionWorkshop/Models/AuthenticationService.cs
+++ DependencyInjectionWorkshop/Models/AuthenticationService.cs
@@ -1,48 +1,93 @@
-﻿using System;
+﻿using Dapper;
+using SlackAPI;
+using System;
 using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
-using Dapper;
 
 namespace DependencyInjectionWorkshop.Models
 {
     public class AuthenticationService
     {
-        public bool Verify(string accountId, string pwd, string opt)
+        public bool Verify(string accountId, string password, string otp)
         {
-            var password = string.Empty;
-            using (var connection = new SqlConnection("my connection string"))
+            var httpClient = new HttpClient() { BaseAddress = new Uri("http://joey.com/") };
+
+            //check account locked
+            var isLockedResponse = httpClient.PostAsJsonAsync("api/failedCounter/IsLocked", accountId).Result;
+
+            isLockedResponse.EnsureSuccessStatusCode();
+            if (isLockedResponse.Content.ReadAsAsync<bool>().Result)
             {
-                password = connection.Query<string>("spGetUserPassword", new { Id = accountId },
-                   commandType: CommandType.StoredProcedure).SingleOrDefault();
+                throw new FailedTooManyTimesException() { AccountId = accountId };
             }
 
+            //get password
+            string passwordFromDb;
+            using (var connection = new SqlConnection("my connection string"))
+            {
+                passwordFromDb = connection.Query<string>("spGetUserPassword", new { Id = accountId },
+                                                          commandType: CommandType.StoredProcedure).SingleOrDefault();
+            }
+
+            //hash
             var crypt = new System.Security.Cryptography.SHA256Managed();
             var hash = new StringBuilder();
-            var crypto = crypt.ComputeHash(Encoding.UTF8.GetBytes(pwd));
+            var crypto = crypt.ComputeHash(Encoding.UTF8.GetBytes(password));
             foreach (var theByte in crypto)
             {
                 hash.Append(theByte.ToString("x2"));
             }
 
-            if (password.Equals(hash.ToString()) == false)
-                return false;
+            var hashedPassword = hash.ToString();
 
-            var newOTP = string.Empty;
-            var httpClient = new HttpClient() { BaseAddress = new Uri("http://joey.com/") };
+            //get otp
             var response = httpClient.PostAsJsonAsync("api/otps", accountId).Result;
-            if (response.IsSuccessStatusCode)
-            {
-                newOTP = response.Content.ReadAsAsync<string>().Result;
-            }
-            else
+            if (!response.IsSuccessStatusCode)
             {
                 throw new Exception($"web api error, accountId:{accountId}");
             }
 
-            return newOTP.Equals(opt);
+            var currentOtp = response.Content.ReadAsAsync<string>().Result;
+
+            //compare
+            if (passwordFromDb == hashedPassword && currentOtp == otp)
+            {
+                var resetResponse = httpClient.PostAsJsonAsync("api/failedCounter/Reset", accountId).Result;
+                resetResponse.EnsureSuccessStatusCode();
+
+                return true;
+            }
+            else
+            {
+                //失敗
+                var addFailedCountResponse = httpClient.PostAsJsonAsync("api/failedCounter/Add", accountId).Result;
+                addFailedCountResponse.EnsureSuccessStatusCode();
+
+                //紀錄失敗次數
+                var failedCountResponse =
+                    httpClient.PostAsJsonAsync("api/failedCounter/GetFailedCount", accountId).Result;
+
+                failedCountResponse.EnsureSuccessStatusCode();
+
+                var failedCount = failedCountResponse.Content.ReadAsAsync<int>().Result;
+                var logger = NLog.LogManager.GetCurrentClassLogger();
+                logger.Info($"accountId:{accountId} failed times:{failedCount}");
+
+                //notify
+                string message = $"account:{accountId} try to login failed";
+                var slackClient = new SlackClient("my api token");
+                slackClient.PostMessage(messageResponse => { }, "my channel", message, "my bot name");
+
+                return false;
+            }
         }
+    }
+
+    public class FailedTooManyTimesException : Exception
+    {
+        public string AccountId { get; set; }
     }
 }
